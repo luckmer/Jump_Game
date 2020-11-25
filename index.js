30621721a9e645e3443cb1d456f0430048fb4eef
luckmer <woszczo04@gmail.com>
2020-11-25 21:43:51 +0100

    js update

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ let active = test
 let TestLeft = left;
 let table = [];
 
-const SS = function(Gap){
-    this.left  = Math.random() * 315  ; 
+ function SS (Gap){
+    this.left = Math.floor(Math.random() * 4) * 70;
     this.bottom = Gap;
     this.visual = document.createElement('div');
     const visual = this.visual;
@@ -26,14 +26,15 @@ const SS = function(Gap){
     game.appendChild(visual)
 }
 
-const  CreatePlatform = () =>  {
-    let plat = [0,1,2,3,4];
+const CreatePlatform = () =>
+{
+    let plat = [0,1,2,3,4,5];
     plat.forEach(i =>{
         let Gap = 100 + i * 120
         let test = new SS(Gap);
         table.push(test)
     })
-}
+};
 
 const Platform = () =>{
     for (let i = 0; i < table.length; i++){
@@ -42,19 +43,21 @@ const Platform = () =>{
     }
 }
 
-const  JumpCharacter =() => {
-    let upTimerId = setInterval(function (){
+const JumpCharacter = () =>
+{
+    let upTimerId = setInterval(function ()
+    {
         if (bottom > 250) {
             active -= 20;
-
             clearInterval(upTimerId);
-            let downTimerId = setInterval(function () {
+            let downTimerId = setInterval(function ()
+            {
                 if (bottom < 0) {
                     clearInterval(downTimerId);
                 }
                 active -= 40;
                 bottom -= 5;
-                Collision(active,TestLeft);
+                Collision(active, TestLeft,downTimerId);
                 bottom = bottom * gravity;
                 character.style.bottom = bottom + "px";
             }, 20);
@@ -64,85 +67,101 @@ const  JumpCharacter =() => {
         bottom = bottom * gravity;
         character.style.bottom = bottom + "px";
     }, 20);
+
+};
+function MoveDown()
+{
+    bottom -= 10
+    character.style.bottom = bottom + "px"
 }
 
 
-const Collision = (top,TestLeft) =>{
-    table.forEach(item =>{
-        
-        let colider = item.left;
-        let first = table[0].left;
-        let bottom = item.bottom;
-        if (
-            top >= bottom && 
-            top <= bottom + 85 
-        
+const Collision = (top, TestLeft, downTimerId) =>{
+        table.forEach(item =>{
+        let colider = table[0].left;
+        let bottom = item.left;
+            if (
+                top >= bottom &&
+                top <= bottom + 410 && 
+                TestLeft + 40 >= colider &&
+                TestLeft <= colider + 95
         ) {
-            console.log(top,TestLeft)
+           //clearInterval(downTimerId);
         };
-
     });
 };
+
 const MoveLeft = () =>{
-    if (isGoing == false) { 
-        clearInterval(rightTimerId)
-    } 
-    leftTimerId = setInterval(function () {
-        TestLeft -= 5;     
-        TestLeft < 0 ? ClearRightInterval() : [];
-        character.style.left = TestLeft + "px";
-    }, 20);
-}
+    // if (isGoing == false) {
+    //     clearInterval(rightTimerId)
+    // }
+    // leftTimerId = setInterval(function ()
+    // {
+    //     TestLeft -= 5;
+    //     TestLeft < 0 ? ClearRightInterval() : [];
+    //     character.style.left = TestLeft + "px";
+    // }, 20);
+    TestLeft -= 30
+    character.style.left = TestLeft +"px"
+};
 
-const MoveRight = () =>{
-    if (isGoing == false) {
-        clearInterval(leftTimerId)
-    } 
-    rightTimerId = setInterval(function (){
-        TestLeft += 5;
-        TestLeft > 280 ? ClearState() : [];
-        character.style.left = TestLeft + "px";
-    }, 20);    
-}
+const MoveRight = () =>
+{
+    // if (isGoing == false) {
+    //     clearInterval(leftTimerId)
+    // }
+    // rightTimerId = setInterval(function ()
+    // {
+    //     TestLeft += 5;
+    //     TestLeft > 280 ? ClearState() : [];
+    //     character.style.left = TestLeft + "px";
+    // }, 20);
+    TestLeft +=30
+    character.style.left = TestLeft +"px"
+};
 
-const  CreateNewPlatform = (i) => {
+const CreateNewPlatform = (i) =>
+{
     if (table[i].bottom < 10) {
         table[0].visual.classList.remove("platform");
         table.shift();
         let test = new SS(600);
         table.push(test);
     }
-}
+};
 
-const  MovePlatformDown =(i) => {
-    if (active > 20000) {
+const MovePlatformDown = (i) =>
+{
+    if (active > 200) {
         table[i].bottom -= 4;
         table[i].visual.style.bottom = table[i].bottom + "px";
     }
-}
+};
 
-const  Joystick = (e) => {
-    if (e.keyCode === 68 ) {
+const Joystick = (e) =>
+{
+    if (e.keyCode === 68) {
         MoveRight(left);
-    } else if (e.keyCode === 65 ) {
+    } else if (e.keyCode === 65) {
         MoveLeft(left);
     } else if (e.keyCode === 32) {
         JumpCharacter(test);
+    } else if (e.keyCode === 83) {
+        MoveDown();
     }
-}
+};
 
 function ClearRightInterval(){
     left = 0;
     clearInterval(leftTimerId)
-}
+};
 
 function ClearState(){
     left = 280;
     clearInterval(rightTimerId);
-}
+};
 
 function Start(){
-
 
 }
 
@@ -152,4 +171,3 @@ setInterval(Platform,20)
 
 Play.addEventListener("click", Start);
 document.addEventListener("keyup", Joystick)
-document.addEventListener("keydown", () =>{})
